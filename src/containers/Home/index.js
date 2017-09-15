a1a9a272aabe335739c009ead6a316bb6345cb30
shifeng <shifeng199307@gmail.com>
2017-09-15 16:22:54 +0800

    9.15

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,12 +1,40 @@
 import React, {Component} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
+import {bindActionCreators} from 'redux'
+import {connect} from 'react-redux'
 
-export default class Home extends Component {
+// 引入action
+import * as goodsAction from '../../store/actions/goods';
+
+// 常量设置
+const INITIAL_PAGE = 1;
+
+class Home extends Component {
+  constructor(props) {
+    super(props);
+  }
+  componentWillMount() {
+    const {actions} = this.props;
+    actions.getGoodsList(INITIAL_PAGE)
+  }
   render() {
+    console.log(this.props.goodsList.data)
     return (
       <View>
         <Text>主页</Text>
       </View>
-    )
+    );
   }
 }
+
+// 同步store中的state，状态改变，实时更新
+const mapStateToProps = state => {
+  return {goodsList: state.goods.goodsList};
+}
+// 同步store中的action
+const mapDispatchToProps = (dispatch) => ({
+  actions: bindActionCreators(goodsAction, dispatch),
+  dispatch: dispatch
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home)
